37a0fdaabd460830f4614a6bbd652b0151fb35aa
tub454 <107926193+tub454@users.noreply.github.com>
2023-07-26 09:22:37 +0000

    different datatypes in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,39 @@
+
+"use strict"  // treat all js code as newer version
+
+let age = 20
+let firstName = 'Arnab'
+console.log(typeof(age))        // number
+console.log(typeof firstName)   // string
+
+/*
+Primitive Types
+   In JavaScript, a primitive (primitive value, primitive data type) is data that is not an object and has no methods or properties. There are 7 primitive data types:
+     string
+     number
+     bigint
+     boolean
+     undefined
+     Symbol
+     null
+*/
+
+let a;
+console.log(typeof a)     // undefined   
+
+let b = null
+console.log(typeof b)     // object
+// this is a bug, an error, but not changed intentionally
+
+// BigInt
+let myNum;
+console.log(Number.MAX_SAFE_INTEGER)  // 9007199254740991
+myNum = 12345678987654210
+// Numeric literals with absolute values equal to 2^53 or greater are too large to be represented accurately as integers.
+console.log(myNum)  // 12345678987654210      
+myNum = BigInt(12345678987654210)
+console.log(myNum)  // 12345678987654210n
+myNum = 123n
+let myNum2 = 123
+console.log(typeof myNum)    // bigint
+console.log(typeof myNum2)   // number
